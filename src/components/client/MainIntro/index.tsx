f3cc43f9290e17cfe6852e78e50d760f116154a7
a.gorbal <a.gorbal@finch-melrose.com>
2025-07-07 15:06:06 +0300

    update

--- src/components/client/MainIntro/index.tsx
+++ src/components/client/MainIntro/index.tsx
@@ -4,7 +4,7 @@ export const MainIntro = () => {
   return (
     <section className={s.main}>
       <video autoPlay muted playsInline loop className={s.video}>
-        <source src="/maks.MP4" type="video/mp4" media="(min-width: 768px)" />
+        <source src="/maks.mp4" type="video/mp4" media="(min-width: 768px)" />
         <source src="/team.mp4" type="video/mp4" media="(max-width: 767px)" />
       </video>
       <h1 className={s.title}>One team One dream</h1>
