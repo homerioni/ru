1471b58e5e4a7b48fc2c6fe4264136037e090bcb
a.gorbal <a.gorbal@finch-melrose.com>
2025-07-15 12:12:50 +0300

    update

--- src/components/client/NextMatch/index.tsx
+++ src/components/client/NextMatch/index.tsx
@@ -3,6 +3,7 @@ import { NextMatchTimer } from '@/components/client/NextMatch/NextMatchTimer';
 import { TGetMatch } from '@/services/matches';
 import { getMatchDate } from '@/utils/getMatchDate';
 import s from './styles.module.scss';
+import Link from 'next/link';
 
 type NextMatchProps = {
   match: Omit<TGetMatch, 'players'>;
@@ -13,18 +14,23 @@ export const NextMatch = ({ match }: NextMatchProps) => {
 
   return (
     <section className={`${s.main} container`}>
-      <ClubLogo logoSrc={match?.homeClub.logoSrc} name={match?.homeClub.name} />
-      <div className={s.infoBox}>
-        <p className={s.infoTitle}>Следующий матч</p>
-        <NextMatchTimer matchDate={matchDate.timestamp} />
-        <p className={s.date}>{matchDate.day}</p>
-        <p className={s.time}>{matchDate.time}</p>
-      </div>
-      <ClubLogo
-        logoSrc={match?.awayClub.logoSrc}
-        name={match?.awayClub.name}
-        namePosition={NAME_POSITION.LEFT}
-      />
+      <Link href={`/match/${match.id}`}>
+        <ClubLogo
+          logoSrc={match?.homeClub.logoSrc}
+          name={match?.homeClub.name}
+        />
+        <div className={s.infoBox}>
+          <p className={s.infoTitle}>Следующий матч</p>
+          <NextMatchTimer matchDate={matchDate.timestamp} />
+          <p className={s.date}>{matchDate.day}</p>
+          <p className={s.time}>{matchDate.time}</p>
+        </div>
+        <ClubLogo
+          logoSrc={match?.awayClub.logoSrc}
+          name={match?.awayClub.name}
+          namePosition={NAME_POSITION.LEFT}
+        />
+      </Link>
     </section>
   );
 };
