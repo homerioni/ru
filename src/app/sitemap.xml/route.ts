91d26c5ef023702eaf3ffd718a8d03231ecef86b
a.gorbal <a.gorbal@finch-melrose.com>
2025-07-16 10:37:55 +0300

    update

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -14,7 +14,9 @@ export async function GET() {
     ...matchIds.map((id) => `${SITE_URL}/match/${id}`),
   ];
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.map((url) => `<url><loc>${url}</loc><lastmod>${new Date().toISOString().split('T')[0]}</lastmod><changefreq>weekly</changefreq><priority>0.8</priority></url>`).join('')}</urlset>`;
+  const priorities = [1, 0.9, 0.8, 0.9];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.map((url, i) => `<url><loc>${url}</loc><lastmod>${new Date().toISOString().split('T')[0]}</lastmod><changefreq>weekly</changefreq><priority>${priorities[i] ? priorities[i] : '0.7'}</priority></url>`).join('')}</urlset>`;
 
   return new Response(sitemap, {
     headers: {
