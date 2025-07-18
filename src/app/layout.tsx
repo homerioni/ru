e7b28eaecc481db7052ec24c1b57caa5d9af63f0
a.gorbal <a.gorbal@finch-melrose.com>
2025-07-18 11:05:51 +0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from 'next';
+import Head from 'next/head';
 import { Oswald, Rubik } from 'next/font/google';
 import '../assets/Circe/stylesheet.css';
 
@@ -31,6 +32,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo180.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo32.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/logo192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/logo512.png" />
+        <meta property="og:title" content="Речичане United" />
+        <meta
+          property="og:description"
+          content="Любительская футбольная команда города Речица"
+        />
+        <meta property="og:image" content="https://rechutd.ru/logo.png" />
+        <meta property="og:url" content="https://rechutd.ru/" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <body className={`${oswaldSans.variable} ${rubikFont.variable}`}>
         {children}
       </body>
