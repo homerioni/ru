733ab94433259882b2c045566dba65f0f0bedcf3
a.gorbal <a.gorbal@finch-melrose.com>
2025-07-18 11:16:27 +0300

    update

--- src/utils/getMatchDate.ts
+++ src/utils/getMatchDate.ts
@@ -1,10 +1,12 @@
 export const getMatchDate = (date: Date) => {
   const matchDate = new Date(date);
   const day = matchDate.toLocaleDateString('ru-RU', {
+    timeZone: 'Europe/Moscow',
     month: 'long',
     day: 'numeric',
   });
   const time = matchDate.toLocaleTimeString('ru-RU', {
+    timeZone: 'Europe/Moscow',
     hour: '2-digit',
     minute: '2-digit',
   });
