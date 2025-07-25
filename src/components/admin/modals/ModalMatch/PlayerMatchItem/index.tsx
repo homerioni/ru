ac6f7f195e4f423730900550e4aef808180ee94c
a.gorbal <a.gorbal@finch-melrose.com>
2025-07-25 15:54:51 +0300

    update

--- src/components/admin/modals/ModalMatch/PlayerMatchItem/index.tsx
+++ src/components/admin/modals/ModalMatch/PlayerMatchItem/index.tsx
@@ -18,49 +18,51 @@ export const PlayerMatchItem = ({
 }: TPlayerMatchItemProps) => {
   return (
     <Grid gutter={10}>
-      {players.map((player, i) => (
-        <Grid.Col span={{ base: 12, sm: 12 }} key={player.id}>
-          <Grid gutter={10} className={s.team}>
-            <Grid.Col span={{ base: 6, sm: 4 }}>
-              <Input.Wrapper
-                label={i === 0 ? 'Состав на игру' : undefined}
-                className={s.teamLabel}
-              >
-                <div className={s.teamItem}>
-                  <Checkbox
-                    tabIndex={-1}
-                    size="md"
-                    mr="xl"
-                    value={player.id}
-                    {...registerPlayerField(player.id, 'playerId')}
+      {players
+        .sort((a, b) => a.playedIn.length - b.playedIn.length)
+        .map((player, i) => (
+          <Grid.Col span={{ base: 12, sm: 12 }} key={player.id}>
+            <Grid gutter={10} className={s.team}>
+              <Grid.Col span={{ base: 6, sm: 4 }}>
+                <Input.Wrapper
+                  label={i === 0 ? 'Состав на игру' : undefined}
+                  className={s.teamLabel}
+                >
+                  <div className={s.teamItem}>
+                    <Checkbox
+                      tabIndex={-1}
+                      size="md"
+                      mr="xl"
+                      value={player.id}
+                      {...registerPlayerField(player.id, 'playerId')}
+                    />
+                    <p>{player.name}</p>
+                  </div>
+                </Input.Wrapper>
+              </Grid.Col>
+              <Grid.Col span={{ base: 3, sm: 2 }}>
+                <Input.Wrapper label={i === 0 ? 'Голов' : undefined}>
+                  <Input
+                    placeholder="Голов"
+                    type="number"
+                    step={1}
+                    {...registerPlayerField(player.id, 'goals')}
                   />
-                  <p>{player.name}</p>
-                </div>
-              </Input.Wrapper>
-            </Grid.Col>
-            <Grid.Col span={{ base: 3, sm: 2 }}>
-              <Input.Wrapper label={i === 0 ? 'Голов' : undefined}>
-                <Input
-                  placeholder="Голов"
-                  type="number"
-                  step={1}
-                  {...registerPlayerField(player.id, 'goals')}
-                />
-              </Input.Wrapper>
-            </Grid.Col>
-            <Grid.Col span={{ base: 3, sm: 2 }}>
-              <Input.Wrapper label={i === 0 ? 'Ассистов' : undefined}>
-                <Input
-                  placeholder="Ассистов"
-                  type="number"
-                  step={1}
-                  {...registerPlayerField(player.id, 'assists')}
-                />
-              </Input.Wrapper>
-            </Grid.Col>
-          </Grid>
-        </Grid.Col>
-      ))}
+                </Input.Wrapper>
+              </Grid.Col>
+              <Grid.Col span={{ base: 3, sm: 2 }}>
+                <Input.Wrapper label={i === 0 ? 'Ассистов' : undefined}>
+                  <Input
+                    placeholder="Ассистов"
+                    type="number"
+                    step={1}
+                    {...registerPlayerField(player.id, 'assists')}
+                  />
+                </Input.Wrapper>
+              </Grid.Col>
+            </Grid>
+          </Grid.Col>
+        ))}
     </Grid>
   );
 };
