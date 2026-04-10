9361f420dcac4a165706eab2c3b88d74a04aa3b0
Artsiom Horbal <gorbalas@eco.rshb.ru>
2026-04-10 12:29:24 +0300

    Обнова

--- src/components/client/Providers/index.tsx
+++ src/components/client/Providers/index.tsx
@@ -7,5 +7,5 @@ type Props = {
 };
 
 export const Providers = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider refetchInterval={60}>{children}</SessionProvider>;
 };
