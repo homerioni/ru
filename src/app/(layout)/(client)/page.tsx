a6432e5a578b2970d9b7989b5f857ec7581af8f3
Artsiom Horbal <gorbalas@eco.rshb.ru>
2026-04-19 00:29:19 +0300

    Обнова

--- src/app/(layout)/(client)/page.tsx
+++ src/app/(layout)/(client)/page.tsx
@@ -10,7 +10,7 @@ import { getMatchType } from '@/services/matchTypes';
 import { LeagueTable } from '@/components/client/LeagueTable';
 import { getTableStats } from '@/utils/getTableStats';
 
-export const revalidate = 1800;
+export const dynamic = 'force-dynamic';
 
 export default async function MainPage() {
   const [nextMatch, matches, players, matchType] = await Promise.all([
