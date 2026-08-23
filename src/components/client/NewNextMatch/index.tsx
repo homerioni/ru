7218cc09d4e8b06def8c9f4cab9f2e59f0cac4f6
homerion <homerion13@gmail.com>
2026-08-23 12:42:11 +0300

    Обнова

--- src/components/client/NewNextMatch/index.tsx
+++ src/components/client/NewNextMatch/index.tsx
@@ -1,7 +1,4 @@
 import Image from 'next/image';
-import { NextMatchTimer } from '@/components/client/NextMatch/NextMatchTimer';
-import { TGetMatch } from '@/services/matches';
-import { getMatchDate } from '@/utils/getMatchDate';
 import dima1 from '@/assets/img/dima1.webp';
 import dima2 from '@/assets/img/dima2.webp';
 import shifu1 from '@/assets/img/shifu1.webp';
@@ -9,13 +6,7 @@ import shifu2 from '@/assets/img/shifu2.webp';
 import nad from '@/assets/img/nad-player.webp';
 import s from './styles.module.scss';
 
-type NextMatchProps = {
-  match: Omit<TGetMatch, 'players'>;
-};
-
-export const NewNextMatch = ({ match }: NextMatchProps) => {
-  const matchDate = getMatchDate(match.date);
-
+export const NewNextMatch = () => {
   return (
     <section className={`${s.main} container`}>
       <div className={s.animate}>
@@ -25,12 +16,6 @@ export const NewNextMatch = ({ match }: NextMatchProps) => {
         <Image src={shifu2} alt={''} />
         <Image src={nad} alt={''} />
       </div>
-      {/*<div className={s.infoBox}>*/}
-      {/*  <p className={s.infoTitle}>Следующий матч</p>*/}
-      {/*  <NextMatchTimer matchDate={matchDate.timestamp} />*/}
-      {/*  <p className={s.date}>{matchDate.day}</p>*/}
-      {/*  <p className={s.time}>{matchDate.time}</p>*/}
-      {/*</div>*/}
     </section>
   );
 };
