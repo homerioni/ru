f7a00381fb75178b7d6e73c065e583b78a6839be
homerion <homerion13@gmail.com>
2026-08-23 13:07:59 +0300

    Обнова

--- src/components/client/NewNextMatch/index.tsx
+++ src/components/client/NewNextMatch/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import dima1 from '@/assets/img/dima1.webp';
 import dima2 from '@/assets/img/dima2.webp';
+import dima3 from '@/assets/img/dima3.webp';
 import shifu1 from '@/assets/img/shifu1.webp';
 import shifu2 from '@/assets/img/shifu2.webp';
 import nad from '@/assets/img/nad-player.webp';
@@ -15,6 +16,7 @@ export const NewNextMatch = () => {
         <Image src={shifu1} alt={''} />
         <Image src={shifu2} alt={''} />
         <Image src={nad} alt={''} />
+        <Image src={dima3} alt={''} />
       </div>
     </section>
   );
