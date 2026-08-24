4344c283551e4642335d0022d54326f8a2edfa22
homerion <homerion13@gmail.com>
2026-08-24 14:47:43 +0300

    Обнова

--- src/app/(layout)/(client)/page.tsx
+++ src/app/(layout)/(client)/page.tsx
@@ -11,7 +11,6 @@ import { getMatchType } from '@/services/matchTypes';
 import { LeagueTable } from '@/components/client/LeagueTable';
 import { getTableStats } from '@/utils/getTableStats';
 import { ClubGallerySlider } from '@/components/client/ClubGallerySlider';
-import { NewNextMatch } from '@/components/client/NewNextMatch';
 import { NextMatch } from '@/components/client/NextMatch';
 
 export const revalidate = 1800;
@@ -36,7 +35,6 @@ export default async function MainPage() {
     <>
       <SiteUpdatesModal />
       <MainIntro />
-      {nextMatch && <NewNextMatch />}
       {nextMatch && <NextMatch match={nextMatch} />}
       <PreviousMatchesSlider matches={matches} clubId={MY_CLUB_ID} />
       <LeagueTable
